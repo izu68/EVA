5fad9f4ede412a3eb266e79c2820991952a3fc82
izu <izu>
2024-10-27 16:32:24 +0100

    exposed registers to control region and changed memory map a bit

--- eva.c
+++ eva.c
@@ -23,35 +23,56 @@ void eva_update_data_port ( void )
 {
 	if ( eva.addr_bank == 0 )
 	{
-		printf ( "(EVA ERROR) UNAUTHORIZED MEM WRITE: BANK %02X ADDR %04X\n", eva.addr_bank, eva.addr );
+		printf ( "(EVA ERROR) Unauthorized access: BANK %02Xn", eva.addr_bank );
 		return;
 	}
 	eva.data = EVA_RAM[0x00][0x0004] << 24 | EVA_RAM[0x00][0x0005] << 16 |
-			 EVA_RAM[0x00][0x0006] << 8 | EVA_RAM[0x00][0x0007];
+		   EVA_RAM[0x00][0x0006] << 8  | EVA_RAM[0x00][0x0007];
 	/* transfer from port to actual memory location */
 	EVA_RAM[eva.addr_bank][eva.addr] = EVA_RAM[0x00][0x0004];
 	EVA_RAM[eva.addr_bank][eva.addr+1] = EVA_RAM[0x00][0x0005];
 	EVA_RAM[eva.addr_bank][eva.addr+2] = EVA_RAM[0x00][0x0006];
 	EVA_RAM[eva.addr_bank][eva.addr+3] = EVA_RAM[0x00][0x0007];
-	printf ( "(EVA) MEM WRITE: BANK %02X ADDR %04X: %08X\n", eva.addr_bank, eva.addr, eva.data );
+	printf ( "(EVA) MEM Write: BANK %02X ADDR %04X: %08X\n", eva.addr_bank, eva.addr, eva.data );
+}
+
+void eva_update_registers ( void )
+{
+	eva.r0 = EVA_RAM[0x00][0x0010] << 24 | EVA_RAM[0x00][0x0011] << 16 |
+		 EVA_RAM[0x00][0x0012] << 8  | EVA_RAM[0x00][0x0013];
+	eva.r1 = EVA_RAM[0x00][0x0014] << 24 | EVA_RAM[0x00][0x0015] << 16 |
+		 EVA_RAM[0x00][0x0016] << 8  | EVA_RAM[0x00][0x0017];
+	eva.r2 = EVA_RAM[0x00][0x0018] << 24 | EVA_RAM[0x00][0x0019] << 16 |
+		 EVA_RAM[0x00][0x001A] << 8  | EVA_RAM[0x00][0x001B];
+	eva.r3 = EVA_RAM[0x00][0x001C] << 24 | EVA_RAM[0x00][0x001D] << 16 |
+		 EVA_RAM[0x00][0x001E] << 8  | EVA_RAM[0x00][0x001F];
+
+	eva.f0 = EVA_RAM[0x00][0x0020] << 24 | EVA_RAM[0x00][0x0021] << 16 |
+		 EVA_RAM[0x00][0x0022] << 8  | EVA_RAM[0x00][0x0023];
+	eva.f1 = EVA_RAM[0x00][0x0024] << 24 | EVA_RAM[0x00][0x0025] << 16 |
+		 EVA_RAM[0x00][0x0026] << 8  | EVA_RAM[0x00][0x0027];
+	eva.f2 = EVA_RAM[0x00][0x0028] << 24 | EVA_RAM[0x00][0x0029] << 16 |
+		 EVA_RAM[0x00][0x002A] << 8  | EVA_RAM[0x00][0x002B];
+	eva.f3 = EVA_RAM[0x00][0x002C] << 24 | EVA_RAM[0x00][0x002D] << 16 |
+		 EVA_RAM[0x00][0x002E] << 8  | EVA_RAM[0x00][0x002F];
 }
 
 void eva_pulse_reset ( void )
 {
-	printf ( "(EVA) pulse reset\n" );
-
 	/* clear first RAM bank */
 	for ( int i = 0; i <= 0xFFFF; i++ )
 	{
 		EVA_RAM[0x00][i] = 0x00;
 	}
-	printf ( "(EVA) interface memory clear\n" );
+	printf ( "(EVA) Interface memory clear\n" );
+
 	/* clear registers */
-	eva.data = eva.r0 = eva.r1 = eva.r2 = eva.r3 = 0x00000000;
-	eva.flags = eva.addr = 0x0000;
-	eva.addr_bank = eva.pc = 0x00;
+	eva.addr_bank = eva.addr = eva.data =
+	eva.r0 = eva.r1 = eva.r2 = eva.r3 =
+	eva.f0 = eva.f1 = eva.f2 = eva.f3 =
+	eva.flags = eva.pc = 0;
 	eva.soft_boot = false;
-	printf ( "(EVA) registers and ports clear\n" );
+	printf ( "(EVA) Registers and ports clear\n" );
 
 	/* stop evasound and parse playback */
 	for ( int i = 0; i <= 0xFF; i++ )
@@ -60,8 +81,8 @@ void eva_pulse_reset ( void )
 		evasound.sound_bank[i].active = false;
 	}
 	evasound_parse_sbt ();
-	printf ( "(EVA) reset done\n" );
-	printf ( "(EVA) CALLING BOOT ROM\n" );
+	printf ( "(EVA) Pulse reset\n" );
+	printf ( "(EVA) Jump: BIOS\n" );
 }
 
 void eva_m68k_reset_feedback ( void )
@@ -113,7 +134,7 @@ void eva_reset ( void )
 
 void eva_bootrom_swaprom ( void )
 {
-	printf ( "(EVA) CALLING GAME ROM\n" );
+	printf ( "(EVA) Jump: GAME\n" );
 	eva.soft_boot = true;
 	m68k_pulse_reset ();
 }
--- eva.h
+++ eva.h
@@ -18,10 +18,13 @@ extern e_byte EVA_RAM[ADDRSPACE 0xFF][ADDRSPACE 0xFFFF];
 	BANK 00: 0002 (A13002): ADDR PORT
 	BANK 00: 0004-0007 (A13004-A13007): DATA PORT
 	
-	BANK 00: 0010-00EF (A13010-A130EF): ECT
-	BANK 00: 00F0 (A130F0): ECT FREE
+	BANK 00: 0010-001F (A13010-A1301F): R0-R3
+	BANK 00: 0020-002F (A13020-A1302F): F0-F3
 
-	BANK 01: 0000-FFFF (3F0000-3FFFFF): VRAM
+	BANK 00: 0070-00EF (A13070-A130EF): ECT
+
+	BANK 01: 0000-FFFF (3E0000-3EFFFF): HRC MIRROR
+	BANK 02: 0000-FFFF (3F0000-3FFFFF): VRAM MIRROR
 */
 
 typedef struct
@@ -42,6 +45,7 @@ extern eva_t eva;
 void eva_update_address_bank ( void );
 void eva_update_address_port ( void );
 void eva_update_data_port ( void );
+void eva_update_registers ( void );
 void eva_pulse_reset ( void );
 void eva_m68k_reset_feedback ( void );
 void eva_cycle ( void );
